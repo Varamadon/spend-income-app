88439e593dce5633e0ad9a1e11e20a638e55b230
daniil.mironov <daniil.mironov@team.wrike.com>
2022-08-06 14:24:42 +0300

    get rid of firebase index.html, get rid of js files, display scratch input for now

--- src/dashboard/Dashboard.tsx
+++ src/dashboard/Dashboard.tsx
@@ -21,6 +21,7 @@ import { mainListItems, secondaryListItems } from './listItems';
 import Chart from './Chart';
 import Deposits from './Deposits';
 import Orders from './Orders';
+import SpendInput from "../spend/SpendInput";
 
 function Copyright(props: any) {
   return (
@@ -165,41 +166,41 @@ function DashboardContent() {
         >
           <Toolbar />
           <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-            <Grid container spacing={3}>
-              {/* Chart */}
-              <Grid item xs={12} md={8} lg={9}>
-                <Paper
-                  sx={{
-                    p: 2,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    height: 240,
-                  }}
-                >
-                  <Chart />
-                </Paper>
-              </Grid>
-              {/* Recent Deposits */}
-              <Grid item xs={12} md={4} lg={3}>
-                <Paper
-                  sx={{
-                    p: 2,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    height: 240,
-                  }}
-                >
-                  <Deposits />
-                </Paper>
-              </Grid>
-              {/* Recent Orders */}
-              <Grid item xs={12}>
-                <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
-                  <Orders />
-                </Paper>
-              </Grid>
-            </Grid>
-            <Copyright sx={{ pt: 4 }} />
+            <SpendInput />
+            {/*<Grid container spacing={3}>*/}
+            {/*  /!* Chart *!/*/}
+            {/*  <Grid item xs={12} md={8} lg={9}>*/}
+            {/*    <Paper*/}
+            {/*      sx={{*/}
+            {/*        p: 2,*/}
+            {/*        display: 'flex',*/}
+            {/*        flexDirection: 'column',*/}
+            {/*        height: 240,*/}
+            {/*      }}*/}
+            {/*    >*/}
+            {/*      <Chart />*/}
+            {/*    </Paper>*/}
+            {/*  </Grid>*/}
+            {/*  /!* Recent Deposits *!/*/}
+            {/*  <Grid item xs={12} md={4} lg={3}>*/}
+            {/*    <Paper*/}
+            {/*      sx={{*/}
+            {/*        p: 2,*/}
+            {/*        display: 'flex',*/}
+            {/*        flexDirection: 'column',*/}
+            {/*        height: 240,*/}
+            {/*      }}*/}
+            {/*    >*/}
+            {/*      <Deposits />*/}
+            {/*    </Paper>*/}
+            {/*  </Grid>*/}
+            {/*  /!* Recent Orders *!/*/}
+            {/*  <Grid item xs={12}>*/}
+            {/*    <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>*/}
+            {/*      <Orders />*/}
+            {/*    </Paper>*/}
+            {/*  </Grid>*/}
+            {/*</Grid>*/}
           </Container>
         </Box>
       </Box>
